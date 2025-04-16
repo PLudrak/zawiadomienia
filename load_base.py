f4811bb57cb6f680abf9c4bd8ed094df0a9f3e91
PLudrak <jakwojcicki97@gmail.com>
2025-04-16 15:08:41 +0200

    kolorki i eksport dzialajacy

--- load_base.py
+++ load_base.py
@@ -1,5 +1,6 @@
 from utils import *
 import pandas as pd
+from colorama import Fore, Style, init
 
 id_pracy = "S17K"
 
@@ -27,8 +28,8 @@ def create_base_from_excel(filename):
 		'KOD_POCZTOWY',
 		'MIASTO'] 
 			
-	df_osoby = pd.DataFrame(columns=osoby_kolumny)
-
+	df = pd.DataFrame(columns=osoby_kolumny)
+	import_message = Fore.GREEN + "Załadowano:" + Style.RESET_ALL
 	kody_pocztowe = {}
 	for index, row in df_import.iterrows():
 		LP = index +1
@@ -43,15 +44,35 @@ def create_base_from_excel(filename):
 		NIP = row['nip']
 		ULICA, NR_DOMU, NR_LOKALU,KOD_POCZTOWY, MIASTO = convert_adress(SWDE_ADRES,kody_pocztowe)
 		
-		#dodaj kod pocztowy do słownika
-		if MIASTO not in kody_pocztowe:
-			kody_pocztowe[MIASTO] = KOD_POCZTOWY
+		nowy_wiersz = {
+		'LP':LP,
+		'ID_ZASTAPIENIA':ID_ZASTAPIENIA,
+		'ID_ORYGINALNE':ID_ORYGINALNE,
+		'SWDE_NAZWA':SWDE_NAZWA, 
+		'SWDE_ADRES':SWDE_ADRES,
+		'IMIĘ_OJCA':IMIE_OJCA,
+		'IMIĘ_MATKI':IMIE_MATKI,
+		'TYP_OSOBY':TYP_OSOBY,
+		'PESEL':PESEL,
+		'NIP':NIP,
+		'NAZWA':SWDE_NAZWA, #
+		'ULICA':ULICA,
+		'NR_DOMU':NR_DOMU,
+		'NR_LOKALU':NR_LOKALU,
+		'KOD_POCZTOWY':KOD_POCZTOWY,
+		'MIASTO':MIASTO
+		}
 		
-		print([LP,ID_ORYGINALNE,SWDE_NAZWA,SWDE_ADRES,IMIE_OJCA,IMIE_MATKI,TYP_OSOBY,ULICA,NR_DOMU,NR_LOKALU,KOD_POCZTOWY,MIASTO])
-
+		df = pd.concat([df, pd.DataFrame([nowy_wiersz])],ignore_index=True)
+		#dodaj kod pocztowy do słownika
+		kody_pocztowe[MIASTO] = KOD_POCZTOWY
+		
+		print(import_message,[LP,ID_ORYGINALNE,SWDE_NAZWA,SWDE_ADRES,IMIE_OJCA,IMIE_MATKI,TYP_OSOBY,ULICA,NR_DOMU,NR_LOKALU,KOD_POCZTOWY,MIASTO])
+	return df
 
 
 
 
 filename=r"D:\Python\kuba\zawiadomienia\import\5.11.2024\wlasc.xlsx"
-create_base_from_excel(filename)
+df = create_base_from_excel(filename)
+df.to_excel("Osoby.xlsx", index=False)
--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 from geopy.geocoders import Nominatim
 import re
-
+from colorama import Fore, Style, init
 
 def create_name(SWDE_nazwa:str, SWDE_imie:str):
 	"""tworzy nazwę Osoby: NAZWISKO + IMIĘ(jeżeli imię występuje)"""
@@ -44,7 +44,7 @@ def convert_adress(SWDE_adres:str,kody_pocztowe:dict):
 		
 		#wyodrębnianie miejscowości i kodu pocztowego
 		kod_miejscowosc=kod_miejscowosc.split()
-		if len(kod_miejscowosc) < 2 and "-" not in kod_miejscowosc[0]:
+		if len(kod_miejscowosc) < 2 and "-" not in kod_miejscowosc[0]: #Sprawdza czy kod pocztowy nie znajduje się w zmiennej
 			miejscowosc = " ".join(kod_miejscowosc)
 			s_adres= f"{ulica_nra}, {" ".join(kod_miejscowosc)}" #adres do  wyszukiwania kodu pocztowego
 			kod = missing_postcode(" ".join(kod_miejscowosc),s_adres,kody_pocztowe)
@@ -55,31 +55,54 @@ def convert_adress(SWDE_adres:str,kody_pocztowe:dict):
 
 		return ulica,nr_a,nr_l,kod,miejscowosc
 	except Exception as e:
-		print(f"Wystąpił błąd przy przetwarzaniu adresu\n '{SWDE_adres}':{e}")
-		print("Wciśnij '-' jeżeli brak adresu, lub wpisz poprawiony adres w formacie:")
-		print("ULICA NUMER m.LOKAL; KOD MIEJSCOWOŚĆ")
-		adres_input = input()
-		if adres_input.upper() == "-":
-			return pusty_adres
-		ulica,nr_a,nr_l,kod,miejscowosc = convert_adress(adres_input)
-		return ulica,nr_a,nr_l,kod,miejscowosc
+		while True:
+			warning_strong("BRAK ADRESU")
+			print("\n\nWystąpił błąd przy przetwarzaniu adresu")
+			print(Fore.YELLOW + SWDE_adres +Style.RESET_ALL)
+			print("Wpisz poprawiony adres w formacie:")
+			print("ULICA NUMER m.LOKAL; KOD MIEJSCOWOŚĆ lub \n[3] Zatwierdź brak adresu\n[4] Zagranica")
+			adres_input = input(Fore.YELLOW + ">>> " +Style.RESET_ALL)
+			if adres_input.upper() == "3":
+				return pusty_adres
+			elif adres_input == "4":
+				pass
+			elif len(adres_input) < 10:
+				print("Nierozpoznano adresu lub wyobru")
+			else:
+				try:
+					ulica,nr_a,nr_l,kod,miejscowosc = convert_adress(adres_input, kody_pocztowe)
+					return ulica,nr_a,nr_l,kod,miejscowosc
+				except:
+					print("Nierozpoznano adresu lub wyobru")
+def warning(text:str):
+	print()
+	print(Fore.YELLOW + f"! {text} !"+ Style.RESET_ALL)
 
+def warning_strong(text:str):
+	print()
+	print(Fore.RED + f"! {text} !"+ Style.RESET_ALL)
 
 def missing_postcode(miejscowosc:str,adres, kody_pocztowe):
 	"""Uzupełnij kod ręcznie lub wyszukaj automatycznie"""
+	warning("BRAK KODU")
 	print(f"W adresie brak kodu dla miejscowości: {miejscowosc} ")
 	
 	ostatni_kod = kody_pocztowe.get(miejscowosc)
+	while True:
+		print("Wpisz poprawny kod lub:")
+		if ostatni_kod:
+			print(f"[1] Użyj ostaniego kodu dla {miejscowosc}: {kody_pocztowe[miejscowosc]}")
+		print(f"[2] żeby wyszukać {adres}")
+		kod=input(Fore.YELLOW + ">>> " +Style.RESET_ALL)
 		
-	print("Wpisz poprawny kod lub:")
-	if ostatni_kod:
-		print(f"[Y] Użyj ostaniego kodu dla {miejscowosc}: {kody_pocztowe[miejscowosc]}")
-	kod=input("[X] żeby wyszukać\n")
-	
-	if kod.capitalize() == "X":
-		kod = search_postcode(adres)
-	elif kod.capitalize== "Y":
-		kod= kody_pocztowe[miejscowosc]
+		if kod.capitalize() == "2":
+			kod = search_postcode(adres)
+			break
+		elif kod.capitalize()== "1":
+			kod= kody_pocztowe[miejscowosc]
+			break
+		elif is_valid_postcode(kod):
+			break
 	return kod
 
 def search_postcode(miejscowosc:str):
@@ -105,12 +128,26 @@ def search_postcode(miejscowosc:str):
 			postcode = match.group()
 		
 	if postcode:
-		print(f"Kod pocztowy dla adresu {miejscowosc} to {postcode}:\n Y-zatwierdź lub wpisz poprawny kod")
-		zatwierdz = input()
-		if zatwierdz.capitalize() == "Y":
+		print(f"Kod pocztowy dla adresu {miejscowosc} to {postcode}:\n [1] zatwierdź lub wpisz poprawny kod")
+		zatwierdz = input(Fore.YELLOW + ">>> " +Style.RESET_ALL)
+		if zatwierdz.capitalize() == "1":
 			return postcode
 		else:
 			return zatwierdz
 	else: 
 		print("Nie znaleziono kodu")
 	return postcode
+
+def is_valid_postcode(postcode:str):
+	"""Sprawdź czy kod pocztowy jest w formacie NN-NNN"""
+	valid=bool(re.match(r"^\d{2}-\d{3}$",postcode))
+	if valid:
+		return valid
+	else:
+		print("Niepoprawny kod pocztowy, spróbuj ponownie")
+		return valid
+
+if __name__ == "__main__":
+	kody_pocztowe={}
+	is_valid_postcode('05-205')
+	missing_postcode('Klembów',"Żymirskiego 97",kody_pocztowe)
